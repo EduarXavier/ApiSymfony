ca29ca4d1b0d91791495aa3a4981f3267c6e9c02
Eduar Xavier Avendaño <eduar.avendano@gopenux.com>
2023-09-28 11:02:23 -0500

    Declaración de tipado estricto en todas las clases

--- tests/Managers/UserManagerTest.php
+++ tests/Managers/UserManagerTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Managers;
 
 use App\Document\User;
--- tests/Message/NotificationMessageTest.php
+++ tests/Message/NotificationMessageTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Message;
 
 use App\Document\User;
